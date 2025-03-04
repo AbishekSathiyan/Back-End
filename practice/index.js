da2974375775eb442b9443c798bee76d51361c26
AbishekSathiyan <abidinesh2213.gmail.com>
2025-03-04 15:37:44 +0530

    Start Learning BackEnd with Node.Express Creating Servers

---
+++ practice/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3001, () => {
+    console.log("Server started on http://localhost:3001");
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome! Try http://localhost:3001/name, http://localhost:3001/age, or http://localhost:300/address");
+});
+
+app.get("/name", (req, res) => {
+    res.send("I'm Abishek");
+});
+
+app.get("/age", (req, res) => {
+    res.send("I'm just 22 years old");
+});
+
+app.get("/address", (req, res) => {
+    res.send("I'm from Methalodai(South), Methalodai, Ramanathapuram");
+});
